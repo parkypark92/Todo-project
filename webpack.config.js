87c93b48837f5fc4699b944042b917fe68a3e9ee
Andrew <115462877+parkypark92@users.noreply.github.com>
2023-03-05 18:36:15 +0000

    setup project and create basic webpage layout

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    // eslint-disable-next-line no-undef
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "none",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
