591ee5c2c2c54ddae0e0de09db831fa7ec65a2dc
Andrew <115462877+parkypark92@users.noreply.github.com>
2023-04-15 11:41:55 +0100

    remove test tasks

--- dist/main.js
+++ dist/main.js
@@ -1068,31 +1068,6 @@ const Task = (description, date, project, priority) => {
   };
 };
 
-const testTask1 = Task("test", "2023-04-04", "none", "1-high");
-tasks.push(testTask1);
-const testTask2 = Task("test", "2023-04-04", "none", "2-medium");
-tasks.push(testTask2);
-const testTask3 = Task("test", "2023-04-04", "none", "3-low");
-tasks.push(testTask3);
-const testTask4 = Task("test", "2023-04-04", "none", "1-high");
-tasks.push(testTask4);
-const testTask5 = Task("test", "2023-04-04", "none", "2-medium");
-tasks.push(testTask5);
-const testTask6 = Task("test", "2023-04-04", "none", "3-low");
-tasks.push(testTask6);
-const testTask7 = Task("test", "2023-04-04", "none", "1-high");
-tasks.push(testTask7);
-const testTask8 = Task("test", "2023-04-04", "none", "2-medium");
-tasks.push(testTask8);
-const testTask9 = Task("test", "2023-04-04", "none", "3-low");
-tasks.push(testTask9);
-const testTask10 = Task("test", "2023-04-04", "none", "3-low");
-tasks.push(testTask10);
-const testTask11 = Task("test", "2023-04-04", "none", "3-low");
-tasks.push(testTask11);
-const testTask12 = Task("test", "2023-04-04", "none", "3-low");
-tasks.push(testTask12);
-
 
 /***/ }),
 /* 23 */
